6e501ca63ede57ae91cc3a35b4000e49fe0d4c8e
amilivojevic <aleksandra.milivojevic.94@gmail.com>
2016-06-04 20:36:46 +0200

    zavrsen meni

--- Commands/CustomCommands.cs
+++ Commands/CustomCommands.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HCI.GUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,20 @@ namespace HCI.Commands
 {
     public static class CustomCommands
     {
-        public static readonly ICommand testKomanda = new OpenDialogCommand();
+        public static readonly ICommand testKomanda = new OpenPremisesCommand();
+        public static readonly ICommand testKomanda2 = new OpenTypeCommand();
+        public static readonly ICommand testKomanda3 = new OpenTagCommand();
+
+        public static readonly ICommand editPCom = new EditPremisesCommand();
+        public static readonly ICommand editTypeCom = new EditTypeCommand();
+        public static readonly ICommand editTagCom = new EditTagCommand();
+
+        public static readonly ICommand helpCom = new HelpCommand();
+        public static readonly ICommand aboutCom = new AboutCommand();
     }
 
-    public class OpenDialogCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+
+    public class OpenPremisesCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
     {
         public bool CanExecute(object parameter)
         {
@@ -24,7 +35,116 @@ namespace HCI.Commands
 
         public void Execute(object parameter)
         {
-            MessageBox.Show("Hello, world!");
+            PremisesDialog w = new PremisesDialog();
+            w.Show();
+        }
+    }
+
+    public class OpenTypeCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            TypeDialog w = new TypeDialog();
+            w.Show();
+        }
+    }
+
+    public class OpenTagCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            TagsDialog w = new TagsDialog();
+            w.Show();
+        }
+    }
+
+    public class EditPremisesCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            PremisesTableView w = new PremisesTableView();
+            w.Show();
+        }
+    }
+
+    public class EditTypeCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            TypeTableView w = new TypeTableView();
+            w.Show();
+        }
+    }
+
+    public class EditTagCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            TagTableView w = new TagTableView();
+            w.Show();
+        }
+    }
+    public class HelpCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Help");
+        }
+    }
+    public class AboutCommand : ICommand //da hocem da radimo i sa precicama, trebalo bi da nasledom RoutedUICommand, recimo
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("About");
         }
     }
 }
